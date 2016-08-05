b4f855b985bef798a556e77f2dc8f6e51c942d7f
abasile <antoine.basile@gmail.com>
2016-08-05 15:21:31 +0200

    deactivate gmail verif

--- lib/ant.groovy
+++ lib/ant.groovy
@@ -43,9 +43,9 @@ def sendmail(param) {
  * sendGmail(subject:'Hello Ant World', message:'Dear Ant, ...', to:'someone@gmail.com', user:'rednoah@gmail.com', password:'correcthorsebatterystaple')
  */
 def sendGmail(param) {
-	if (!(param.password ==~ /\w{16}/)) {
-		throw new IllegalArgumentException('Gmail password must be a 16-digit passcode: Application-specific password required')
-	}
+//	if (!(param.password ==~ /\w{16}/)) {
+//		throw new IllegalArgumentException('Gmail password must be a 16-digit passcode: Application-specific password required')
+//	}
 
 	param << [mailhost: 'smtp.gmail.com', mailport: '587', ssl: 'no', enableStartTLS: 'yes']
 	param << [from: param.user]
